47c43b82f09578640bae0576a346642e63a3a008
kenji-fujisawa <uhimania@uhimanianoMacBook-Air.local>
2025-09-24 19:58:59 +0900

    initial commit

---
+++ TimeCard/TimeCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeCardApp.swift
+//  TimeCard
+//
+//  Created by uhimania on 2025/09/24.
+//
+
+import SwiftUI
+
+@main
+struct TimeCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimeCardTests/TimeCardTests.swift
@@ -0,0 +1,16 @@
+//
+//  TimeCardTests.swift
+//  TimeCardTests
+//
+//  Created by uhimania on 2025/09/24.
+//
+
+import Testing
+
+struct TimeCardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
