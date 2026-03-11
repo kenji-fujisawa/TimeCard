49cf3986e36a8583de65b316c876744a64bbc406
kenji-fujisawa <uhimania@uhimanianoMacBook-Air.local>
2026-03-11 16:45:04 +0900

    バリデーション追加

--- TimeCard/UI/Editor/TimeRecordEditView.swift
+++ TimeCard/UI/Editor/TimeRecordEditView.swift
@@ -15,7 +15,8 @@ struct TimeRecordEditView: View {
             SidebarView(viewModel: viewModel)
         } detail: {
             if let index = viewModel.records.firstIndex(where: { $0.id == viewModel.selectedId }) {
-                DetailView(record: viewModel.records[index])
+                let record = viewModel.records[index]
+                DetailView(record: record, valid: viewModel.isValid(record))
             }
         }
         .frame(minWidth: 400, minHeight: 300)
@@ -30,6 +31,7 @@ struct TimeRecordEditView: View {
                     ForEach(viewModel.records) { record in
                         HStack {
                             Text(record.checkIn.formatted(.dateTime.hour().minute()))
+                                .foregroundStyle(!viewModel.isValid(record) ? .red : .primary)
                                 .accessibilityIdentifier("nav_link")
                             
                             Spacer()
@@ -66,13 +68,16 @@ struct TimeRecordEditView: View {
     
     private struct DetailView: View {
         @Bindable var record: TimeRecordEditViewModel.TimeRecord
+        let valid: Bool
         
         var body: some View {
             VStack(alignment: .leading) {
                 Form {
                     DatePicker("出勤", selection: $record.checkIn, displayedComponents: [.date, .hourAndMinute])
+                        .isValid(valid)
                         .accessibilityIdentifier("date_check_in")
                     DatePicker("退勤", selection: $record.checkOut, displayedComponents: [.date, .hourAndMinute])
+                        .isValid(valid)
                         .accessibilityIdentifier("date_check_out")
                 }
                 .padding()
@@ -80,7 +85,7 @@ struct TimeRecordEditView: View {
                 List {
                     ForEach(record.breakTimes) { breakTime in
                         Section {
-                            BreakTimeView(breakTime: breakTime)
+                            BreakTimeView(breakTime: breakTime, valid: record.isValid(breakTime))
                         } header: {
                             HStack {
                                 Text("休憩")
@@ -119,15 +124,38 @@ struct TimeRecordEditView: View {
     
     private struct BreakTimeView: View {
         @Bindable var breakTime: TimeRecordEditViewModel.BreakTime
+        let valid: Bool
         
         var body: some View {
             Form {
                 DatePicker("開始", selection: $breakTime.start, displayedComponents: [.date, .hourAndMinute])
+                    .isValid(valid)
                     .accessibilityIdentifier("date_break_start")
                 DatePicker("終了", selection: $breakTime.end, displayedComponents: [.date, .hourAndMinute])
+                    .isValid(valid)
                     .accessibilityIdentifier("date_break_end")
             }
         }
+    }
+}
+
+private struct ValidModifier: ViewModifier {
+    let valid: Bool
+    
+    func body(content: Content) -> some View {
+        if valid {
+            content
+        } else {
+            content
+                .padding(2)
+                .border(.red)
+        }
+    }
+}
+
+private extension View {
+    func isValid(_ valid: Bool) -> some View {
+        self.modifier(ValidModifier(valid: valid))
     }
 }
 
