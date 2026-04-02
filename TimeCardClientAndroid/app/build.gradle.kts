444c55eb11d4d42e88f31bbb478eeae6f8377a5c
kenji-fujisawa <uhimania@uhimanianoMacBook-Air.local>
2026-04-02 17:35:03 +0900

    v3.2

--- TimeCardClientAndroid/app/build.gradle.kts
+++ TimeCardClientAndroid/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "jp.uhimania.timecardclientandroid"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "3.1"
+        versionCode = 3
+        versionName = "3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
