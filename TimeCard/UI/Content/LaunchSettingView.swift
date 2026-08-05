9a0cb04d482339f0aba2263281d9ca6cc6f38da3
kenji-fujisawa <kenji_fujisawa@icloud.com>
2026-08-05 12:18:47 +0900

    launchAtLogin の値が正しく取得できてなかったのを修正

--- TimeCard/UI/Content/LaunchSettingView.swift
+++ TimeCard/UI/Content/LaunchSettingView.swift
@@ -11,10 +11,6 @@ import SwiftUI
 struct LaunchSettingView: View {
     @State private var launchAtLogin: Bool = false
     
-    init() {
-        launchAtLogin = SMAppService.mainApp.status == .enabled
-    }
-    
     var body: some View {
         Form {
             Toggle(isOn: Binding<Bool>(
@@ -26,6 +22,9 @@ struct LaunchSettingView: View {
             .toggleStyle(.switch)
         }
         .padding()
+        .onAppear() {
+            launchAtLogin = SMAppService.mainApp.status == .enabled
+        }
     }
     
     private func toggleLaunchAtLogin(value: Bool) {
