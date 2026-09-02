2cd376977048443636ab3637ce83a7580ab70d48
kenji-fujisawa <kenji_fujisawa@icloud.com>
2026-09-02 17:34:20 +0900

    ログイン画面に戻った際に入力値が残らないようにする

--- TimeCardClientIOS/TimeCardClientIOS/UI/LoginViewModel.swift
+++ TimeCardClientIOS/TimeCardClientIOS/UI/LoginViewModel.swift
@@ -50,6 +50,8 @@ class LoginViewModel {
             
             do {
                 try await repository.login(mail: mail, password: password)
+                self.mail = ""
+                self.password = ""
                 self.isLoggedIn = repository.isLoggedIn()
             } catch {
                 self.error = "メールアドレス、またはパスワードが間違っています"
@@ -101,6 +103,9 @@ class LoginViewModel {
             
             do {
                 try await repository.verify(mail: mail, verifyCode: verifyCode)
+                self.mail = ""
+                self.password = ""
+                self.passwordConfirm = ""
                 self.isLoggedIn = repository.isLoggedIn()
             } catch {
                 self.error = error.localizedDescription
